82c7d0e7715093ba8a5e1f5313b529746b571ef1
Konik-Rahman2626 <103890241+Konik-Rahman2626@users.noreply.github.com>
2022-07-27 14:14:28 +0600

    Update connection.php

--- connection.php
+++ connection.php
@@ -3,9 +3,10 @@
 $dbhost = "localhost";
 $dbuser = "root";
 $dbpass = "";
+$dbname="login_sample_db";
 
 
-if($con = mysqli_connect($dbhost,$dbuser,$dbpass,$dbname))
+if(!$con = mysqli_connect($dbhost, $dbuser, $dbpass, $dbname))
 {
 
 	die("failed to connect!");
